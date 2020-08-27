7a585202d601b05cff058c2b85d6c97690fd2956
maninthebit <biburakisik@gmail.com>
2020-08-27 23:37:38 +0300

    Change log4j bundle with slf4j

--- Slf4j/src/com/burakisik/osgi/log/LogComponent.java
+++ Slf4j/src/com/burakisik/osgi/log/LogComponent.java
@@ -1,21 +1,20 @@
 package com.burakisik.osgi.log;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component
 public class LogComponent {
-	   
+	
+	final Logger logger = LoggerFactory.getLogger(LogComponent.class);
+
 	@Activate
 	public void activate() {
-		System.setProperty("log4j.configurationFile",  "log4j.xml");
-	    final Logger logger = LogManager.getLogger();
 		logger.info("test-log4j");
-		//readConfig();
-		
 	}
 
 	@Deactivate
